cc87df2c7bc0829af47683a54d9bad4bb752f2f3
Anantha Debnath <ananthadebnath103@gmail.com>
2025-08-25 12:57:36 +0600

    initialized the project with all setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Home from "../Components/Pages/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+]);
+
+
